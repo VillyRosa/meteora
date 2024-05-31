3e61ac94377145ec558bbf4687e1b2d6c5cddc63
VillyRosa <villyrosa@hotmail.com>
2024-05-31 17:30:50 -0300

    first commit

---
+++ src/app/api/categorias/route.js
@@ -0,0 +1,7 @@
+import { getCategorias } from "@/lib/api";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const categorias = getCategorias();
+  return NextResponse.json(categorias);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { getCategorias, getTodosProdutos } from "../lib/api";
+
+async function fetchApiData() {
+  const urls = ['http://localhost:3000/api/produtos', 'http://localhost:3000/api/categorias'];
+
+  let [produtosRes, categoriasRes] = await Promise.all(urls.map(url => fetch(url)));
+
+  if (!produtosRes.ok || !categoriasRes.ok) {
+    throw new Error('Não foi possível obter os dados');
+  }
+
+  const produtos = await produtosRes.json();
+  const categorias = await categoriasRes.json();
+
+  return { produtos, categorias };
+}
 
 export default async function Home() {
-  const produtos = getTodosProdutos();
-  const categorias = getCategorias();
+  const { produtos, categorias } = await fetchApiData();
 
   return (
     <>
